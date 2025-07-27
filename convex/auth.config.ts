9f22d8fa1587239bf24bc99c4b0fbbd3fee549f2
unknown <arefin.qatek@gmail.com>
2025-07-27 16:11:31 +0600

    prod ready

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,15 +1,8 @@
-// export default {
-//     providers: [
-//         {
-//             domain: process.env.CONVEX_SITE_URL,
-//             applicationID: "convex",
-//         },
-//     ],
-// };
-
-
-
-
 export default {
-    providers: [],
+    providers: [
+        {
+            domain: process.env.CONVEX_SITE_URL,
+            applicationID: "convex",
+        },
+    ],
 };
